92f3b069e3163904b5b7482a3c3e9d64e1bff9c6
Pablo <pablohuggem@gmail.com>
2022-08-14 22:12:23 -0300

    Change a lot of things

--- exact_scripts/executable_lock_n_off.sh
+++ exact_scripts/executable_lock_n_off.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # xset dpms force off & sleep 0.5 && slock
-cat /home/pablo/.ssh/Banners/lock.txt | xargs -0 -I {} physlock -p "{}Artix Linux $(uname -r)"
+cat /home/pablo/.ssh/Banners/capy.txt | xargs -0 -I {} physlock -p "{}Artix Linux $(uname -r)"
